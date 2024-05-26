69454bfc6f08b645f692e36da4f3f03f218902fe
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-05-26 11:11:15 +0200

    aider: Added optional arg for a txt input file that will be read line by line on init.
    # Aider chat conversation:
    
    USER: add optional arg for a txt input file that will be read line by line on init
    
    ASSISTANT: To add an optional argument for a text input file that will be read line by line on initialization, we need to:
    
    1. Modify the `__init__` method to accept an optional file path.
    2. Read the file line by line if the file path is provided.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    litoy.py
    ```python
    <<<<<<< SEARCH
        def __init__(self):
            pass
    =======
        def __init__(self, input_file=None):
            self.lines = []
            if input_file:
                with open(input_file, 'r') as file:
                    self.lines = file.readlines()
    >>>>>>> REPLACE
    ```

--- litoy.py
+++ litoy.py
@@ -1,8 +1,11 @@
 import fire
 
 class LiTOY:
-    def __init__(self):
-        pass
+    def __init__(self, input_file=None):
+        self.lines = []
+        if input_file:
+            with open(input_file, 'r') as file:
+                self.lines = file.readlines()
 
 if __name__ == "__main__":
     fire.Fire(LiTOY)
