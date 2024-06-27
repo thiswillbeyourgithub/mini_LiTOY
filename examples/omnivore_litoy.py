6942efae738f5202f9b6d1fa0bc322b0eccfd1f1
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-06-27 16:37:21 +0200

    minor

--- examples/omnivore_litoy.py
+++ examples/omnivore_litoy.py
@@ -18,7 +18,11 @@ from omnivoreql import OmnivoreQL, CreateLabelInput
 from mini_LiTOY import mini_LiTOY
 
 # Configure logging
-logging.basicConfig(filename='omnivore_logs.txt', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    filename='omnivore_logs.txt',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+)
 log = logging.getLogger()
 
 
